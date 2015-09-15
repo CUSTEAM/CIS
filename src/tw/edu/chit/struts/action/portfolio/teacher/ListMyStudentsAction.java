98d65c90dbc88354fb62a624c074847f3828883b
liuj101 <liuj101@cc.cust.edu.tw>
2015-09-15 13:35:38 +0800

    修正：授課學生列表的學號排序

--- src/tw/edu/chit/struts/action/portfolio/teacher/ListMyStudentsAction.java
+++ src/tw/edu/chit/struts/action/portfolio/teacher/ListMyStudentsAction.java
@@ -84,23 +84,23 @@ public class ListMyStudentsAction extends BaseLookupDispatchAction{
 					students=manager.ezGetBy("SELECT e.path, s.student_no, st.student_name, st.Email, st.entrance, st.schl_code FROM " +
 							"ScoreHist s, stmd st LEFT OUTER JOIN Eps_user e ON e.Uid=st.student_no " +
 							"WHERE s.student_no=st.student_no AND s.school_year='"+year+"' AND s.school_term='"+term+"' AND " +
-							"s.stdepart_class='"+depart_class+"' AND cscode='"+cscode+"'");				
+							"s.stdepart_class='"+depart_class+"' AND cscode='"+cscode+"' ORDER BY st.student_no");				
 					
 					students.addAll(manager.ezGetBy("SELECT e.path, s.student_no, st.student_name, st.Email, st.entrance, st.schl_code FROM " +
 							"ScoreHist s, Gstmd st LEFT OUTER JOIN Eps_user e ON e.Uid=st.student_no " +
 							"WHERE s.student_no=st.student_no AND s.school_year='"+year+"' AND s.school_term='"+term+"' AND " +
-							"s.stdepart_class='"+depart_class+"' AND cscode='"+cscode+"'"));
+							"s.stdepart_class='"+depart_class+"' AND cscode='"+cscode+"' ORDER BY st.student_no"));
 					
 				}else{//以開課
 					String depart_class=request.getParameter("depart_class");					
 					if(request.getParameter("tutor")==null){
 						students=manager.ezGetBy("SELECT e.path, st.student_no, st.student_name, st.Email, st.entrance, st.schl_code FROM " +
 								"stmd st LEFT OUTER JOIN Eps_user e ON e.Uid=st.student_no, Seld d WHERE d.student_no=st.student_no AND " +
-								"d.Dtime_oid='"+depart_class+"'");
+								"d.Dtime_oid='"+depart_class+"' ORDER BY st.student_no");
 					}else{
 						//以導師
 						students=manager.ezGetBy("SELECT e.path, st.student_no, st.student_name, st.Email, st.entrance, st.schl_code FROM " +
-								"stmd st LEFT OUTER JOIN Eps_user e ON e.Uid=st.student_no WHERE st.depart_class='"+depart_class+"'");
+								"stmd st LEFT OUTER JOIN Eps_user e ON e.Uid=st.student_no WHERE st.depart_class='"+depart_class+"' ORDER BY st.student_no");
 						
 					}					
 				}
